1f0be1908f8f099218de9cece4e447163041ec81
Yaakov Aglamaz <yaakova@tingz.co.il>
2024-01-04 15:47:02 +0200

    convert azure file for app settings (json).

---
+++ utils/azure_to_json.ts
@@ -0,0 +1,20 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = '';
+
+rl.on('line', (line: string) => {
+    input += line + '\n';
+});
+
+rl.on('close', () => {
+    // process input
+    const vars = JSON.parse(input);
+    vars.forEach((definition: any) => {
+        console.log(`"${definition.name}": "${definition.value}",`);
+    })
+});
