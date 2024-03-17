67d3f1491afbb498d0f62246b866db128ec48d97
yaakov <yaakova@tingz.co.il>
2024-03-17 14:38:43 +0200

    Upadate install + hours

--- utils/hours.js
+++ utils/hours.js
@@ -1,7 +1,14 @@
-const year = 2024;
-const month = 1;
-let sum = 0;
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1;
+const day = currentDate.getDate();
 
+let sum = 0;
+let sumUpNow = 0;
+const months = [
+  "z", "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 function isWeekend(day) {
   return day === 5 || day === 6; // Sunday is 0, Saturday is 6
 }
@@ -10,21 +17,24 @@ function number_of_month(year, month) {
   return new Date(year, month, 0).getDate();
 }
 
-
 console.log("number of days in month: " + number_of_month(year, month));
 
 for (let i = 1; i <= number_of_month(year, month); i++) {
   if (isWeekend(new Date(year, month - 1, i).getDay())) {
     continue; // Skip if it's Friday or Saturday
   }
+  let dayHours = 0;
 
   if (new Date(year, month - 1, i).getDay() >= 0 && new Date(year, month - 1, i).getDay() <= 3) {
-    sum += 9; // Add 9 hours for Sunday to Wednesday
+    dayHours = 9; // Add 9 hours for Sunday to Wednesday
 	console.log(`${i} 9`);
   } else {
 	console.log(`${i} 8`);
-    sum += 8; // Add 8 hours for Thursday
+    dayHours = 8; // Add 8 hours for Thursday
   }
+  sum+=dayHours;
+  if (i < day) sumUpNow += dayHours;
 }
 
-console.log("Total working hours for January 2024:", sum);
+console.log(`Total working hours for ${months[month]} ${year}:`, sum);
+console.log(`Total upto now: ${sumUpNow}`);
